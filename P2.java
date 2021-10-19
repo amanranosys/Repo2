97549e52d8a48a55f6ffb8208a48cb655c47bd95
amanranosys <92563310+amanranosys@users.noreply.github.com>
2021-10-19 10:06:22 +0530

    Done

--- P2.java
+++ P2.java
@@ -1,1 +1,39 @@
-# Repo2
+class Employee
+{
+  private String name;
+  private int age;
+  private int salary;
+  
+  public void read()
+  {
+    
+  }
+  
+  public void show()
+  {
+    System.out.println("name is"+ name + "age is"+ age + "salary is"+ salary );
+  }
+  
+  public Employee()
+  {
+    
+  }
+  
+  public Employee(String name, int age, int salary)
+  {
+      this.name= name;
+      this.age= age;
+      this.salary= salary;
+  }
+  
+  public class Main
+  {
+    public Static void main(String [] Args)
+    {
+      Employee E1 = new Employee();
+      E1.Employee("Ram", 25, 50000);
+      E1.show();
+    }
+    
+    
+  }
